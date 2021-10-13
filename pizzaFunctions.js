7c86cafc5220a3dee179c885b37ce9fc1a3cc48a
charismabonus <92447164+charismabonus@users.noreply.github.com>
2021-10-13 09:34:57 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,11 @@
+/*
+ * function set to change background color
+ */
+
+function setTomato() {
+    pageTitle.style.background = "tomato";
+}
+
+function setWheat() {
+    pageTitle.style.background = "wheat";
+}
